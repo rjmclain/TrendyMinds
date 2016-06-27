2df33893507d7ac67d9d7d9165bf780f53fbac71
rjmclain <rjmclain88@gmail.com>
2016-06-26 20:21:57 -0500

    Addition of basic files
    Adding all needed files, although final pass, possible background
    images, and a Readme may still be needed

---
+++ Front-end Developer Test/Front-end Developer Test/README.md
@@ -0,0 +1,12 @@
+# Front-end Developer Test
+
+Attached are a group of images that should help you build out this single page. Please use whatever tech stack you are most comfortable with, but refrain from minifying your HTML, CSS and JavaScript.
+
+## Requirements
+
+- Mobile-first
+- IE 10+ compatibility
+
+## Questions
+
+Please reach out if you have any questions at all.
---
+++ js/main.js
@@ -0,0 +1,45 @@
+﻿function navMenuInit() {
+    $('#main-nav #menu-btn').on('click', function () {
+        $('#main-nav').toggleClass('open');
+        /*
+        Also considered making sure all expandable items within the menu would close
+        upon closing the menu itself, but left out in case of accidental
+        menu close so user wouldn't need to restart the process
+        */
+    });
+
+    $('#main-nav .expandable').on('click', function () {
+        if ($(this).hasClass('open')) {
+            $('#main-nav .expandable').removeClass('open');
+        } else {
+            $('#main-nav .expandable').removeClass('open');
+            $(this).addClass('open');
+        }
+    });
+
+    $(window).resize(function () {
+        if ($(window).width() < 781) {
+            $('#main-nav .expandable').removeClass('open');
+            $('#main-nav').removeClass('open');
+        }
+    })
+
+}
+
+
+function searchInit() {
+    $('#top-nav .search-btn').on('click', function () {
+        $('.search-overlay').fadeIn();
+
+    });
+    $('.search-overlay .close-btn').on('click', function () {
+        $('.search-overlay').fadeOut();
+    })
+}
+
+
+
+$(document).ready(function () {
+    navMenuInit();
+    searchInit();
+})
